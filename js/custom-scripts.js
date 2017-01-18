c796a21938329d3ac3f052b619a3e1c7bb4b5287
bIropka <biropka@gmail.com>
2017-01-18 02:10:15 +0200

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
 
+    if ($(window).width() < '981'){
+
+        $('.sign-block').appendTo('nav');
+
+    } else {
+
+        $('.sign-block').appendTo('header-top');
+
+    }
+
+    $(window).resize(function(){
+
+        if ($(window).width() < '981'){
+
+            $('.sign-block').appendTo('nav');
+
+        } else {
+
+            $('.sign-block').appendTo('.header-top');
+
+        }
+
+    });
+
+    $('.fa-bars').click(function() {
+
+        $('nav').addClass('active');
+
+    });
+
+    $('.icon-close').click(function() {
+
+        $('nav').removeClass('active');
+
+    });
+
 });
